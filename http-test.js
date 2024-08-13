260b8905c442988f01a730be8c626f40d323b297
Rajesh Pillai <rajesh@algorisys.com>
2024-08-13 18:38:08 +0530

    Axios simple alternative for learning and poc's

---
+++ http-test.js
@@ -0,0 +1,14 @@
+// Import the HttpClient class
+const HttpClient = require('./http-client');
+
+// Instantiate the HttpClient with the base URL
+const client = new HttpClient('https://jsonplaceholder.typicode.com');
+
+// Fetching data from /posts endpoint
+client.get('/posts')
+    .then(response => {
+        console.log('Response:', response.data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
